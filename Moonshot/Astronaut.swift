4e1d7a9806fb427e81799dd5346e24585e2742d8
Filippo Cilia <45493612+cilippofilia@users.noreply.github.com>
2022-08-19 10:16:42 +0100

    assets and few bits of app

---
+++ Moonshot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Filippo Cilia on 19/08/2022.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
