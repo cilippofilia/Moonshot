b067e97656c7f96511c0aa3a44ede518d1779f75
Filippo Cilia <45493612+cilippofilia@users.noreply.github.com>
2022-08-19 11:38:54 +0100

    final commit before wrap up and challenges

---
+++ Moonshot/AstronautView.swift
@@ -0,0 +1,38 @@
+//
+//  AstronautView.swift
+//  Moonshot
+//
+//  Created by Filippo Cilia on 19/08/2022.
+//
+
+import SwiftUI
+
+struct AstronautView: View {
+    let astronaut: Astronaut
+
+    var body: some View {
+        ScrollView {
+            VStack {
+                Image(astronaut.id)
+                    .resizable()
+                    .scaledToFit()
+
+                Text(astronaut.description)
+                    .foregroundColor(.white)
+                    .padding()
+
+            }
+        }
+        .background(.darkBackground)
+        .navigationTitle(astronaut.name)
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+struct AstronautView_Previews: PreviewProvider {
+    static let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
+
+    static var previews: some View {
+        AstronautView(astronaut: astronauts["armstrong"]!)
+    }
+}
